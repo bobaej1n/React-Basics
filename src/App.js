f3530fc893db67fa1ffe6e6a2564285d4cbb0778
Hyejin Lee <bo_bae_jin@naver.com>
2022-02-15 01:23:07 +0900

    commit 3

--- src/App.js
+++ src/App.js
@@ -8,18 +8,17 @@ function App() {
   let posts = "강남 고기 맛집";
   let [좋아요, 좋아요변경] = useState(0);
 
-  function 제목바꾸기() {
-    var newArray = [...글제목];
-    newArray[0] = '여자 코트 추천';
-    글제목변경(newArray);
-  }
+  //function 제목바꾸기() {
+  //  var newArray = [...글제목];
+  //  newArray[0] = '여자 코트 추천';
+  //  글제목변경(newArray);
+  //}
 
   return (
     <div className="App">
       <div className='black-nav'>
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className='list'>
         <h3> { 글제목[0] } <span onClick={ ()=>{ 좋아요변경(좋아요 + 1) } }>👍</span> {좋아요} </h3>
         <p>2월 17일 발행</p>
@@ -35,8 +34,21 @@ function App() {
         <p>2월 19일 발행</p>
         <hr/>
       </div>
+
+      <Modal />
+
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
